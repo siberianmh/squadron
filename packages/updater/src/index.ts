c66ad75b012ea0597f3a30821bce8c68b8be3c88
Finn Kohn <sokrox@outlook.com>
2021-11-16 02:08:03 +0200

    feat(squadron): init updater
    GitOrigin-RevId: 5c338ac5cf576c653eaa73ae75af40515e994719

---
+++ packages/updater/src/index.ts
@@ -0,0 +1,67 @@
+import { app, autoUpdater } from 'electron'
+
+export interface IOptions {
+  readonly appSlug: string
+  readonly host?: string
+}
+
+class Updater {
+  private host: string = 'https://api.squadron.dev'
+  private appSlug: string | null = null
+
+  public constructor(opts: IOptions) {
+    if (opts.host) {
+      this.host = opts.host
+    }
+
+    this.appSlug = opts.appSlug
+
+    this.initUpdater()
+  }
+
+  private initUpdater() {
+    return app.isReady()
+      ? this.useUpdater()
+      : app.whenReady().then(() => this.useUpdater())
+  }
+
+  private useUpdater() {
+    const feedURL = `${this.host}/apps/${this.appSlug}/${process.platform}/${process.arch}`
+
+    autoUpdater.setFeedURL({
+      url: feedURL,
+    })
+
+    autoUpdater.on('error', (err) => {
+      console.log(`Error during executing: ${err}`)
+    })
+
+    autoUpdater.on('checking-for-update', () => {
+      console.log('checking for update')
+    })
+
+    autoUpdater.on('update-available', () => {
+      console.log('update available')
+    })
+
+    autoUpdater.on(
+      'update-downloaded',
+      (event, releaseNotes, releaseName, releaseDate, updateURL) => {
+        console.log('update downloaded', [
+          event,
+          releaseNotes,
+          releaseName,
+          releaseDate,
+          updateURL,
+        ])
+      },
+    )
+
+    autoUpdater.checkForUpdates()
+    setInterval(() => {
+      autoUpdater.checkForUpdates()
+    }, 5 * 60 * 1000)
+  }
+}
+
+export const updater = (opts: IOptions) => new Updater(opts)
